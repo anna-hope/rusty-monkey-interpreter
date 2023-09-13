b6ce968be63902395bfeef1916fdd219e497a8cb
Anna Melnikov <anna.me@hey.com>
2023-09-13 16:13:37 -0500

    feat: add ability to execute scripts in files
    Add ability to execute a script in a given file.

--- monkey/src/main.rs
+++ monkey/src/main.rs
@@ -1,5 +1,25 @@
-fn main() {
-    println!("This is the Monkey programming language!");
-    println!("Feel free to type in commands");
-    repl::start().unwrap();
+use std::env;
+use std::fs;
+
+use anyhow::{Error, Result};
+
+use interpreter::prelude::*;
+
+fn run_program(input: String) -> Result<()> {
+    let lexer = Lexer::new(input);
+    let mut parser = Parser::new(lexer);
+    let program = parser.parse_program()?;
+    let evaluated = eval_program(&program);
+    println!("{}", evaluated.inspect());
+
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    if let Some(path) = env::args().nth(1) {
+        let input = fs::read_to_string(path)?;
+        run_program(input)
+    } else {
+        repl::start().map_err(Error::from)
+    }
 }
